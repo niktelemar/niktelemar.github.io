a8f3fc26d6559e37ba017e08853054e81ffaa112
Niktelemar <hello@niktelemar.com>
2024-04-26 13:24:45 +0300

    Add hamburger menu

--- Observers.js
+++ Observers.js
@@ -17,3 +17,16 @@ thumbnails.forEach(thumbnail => {
 })
 
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+  addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
